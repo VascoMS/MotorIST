1f53f5bff5d4ee964425d1ed5c0c2b969a3aed8f
MiguelRaposo49456 <A49456@alunos.isel.pt>
2024-12-27 16:56:53 +0000

    Added an extra condition to the install firmware

---
+++ CarServer/src/main/java/sirs/carserver/exception/InstallFirmwareException.java
@@ -0,0 +1,7 @@
+package sirs.carserver.exception;
+
+public class InstallFirmwareException extends Exception {
+    public InstallFirmwareException(String message) {
+        super(message);
+    }
+}
