e48b52fbb7568c6c4411a927ff18e9fe1e14c589
VascoMS <vasco2002silva@gmail.com>
2025-01-02 18:05:04 +0000

    Added DBFiles for docker container and configured tls for communication between db and server.

---
+++ ManufacturerWebServer/src/main/java/sirs/motorist/prototype/config/MongoConfig.java
@@ -0,0 +1,71 @@
+package sirs.motorist.prototype.config;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.ConnectionString;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.SSLContext;
+import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.security.KeyStore;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManager;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${server.ssl.key-store-password}")
+    private String keyStorePassword;
+
+    @Value("${server.ssl.key-store-type}")
+    private String keyStoreType;
+
+    @Value("${dbkeystore.path}")
+    private String keyStorePath;
+
+    @Value("${trust-store}")
+    private String trustStorePath;
+
+    @Bean
+    public MongoClient mongoClient() throws Exception {
+        // Create SSLContext for secure communication
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+
+        // Load your keystore for the client certificate
+        KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        try (InputStream keyStoreStream = getClass().getResourceAsStream(keyStorePath)) {
+            keyStore.load(keyStoreStream, keyStorePassword.toCharArray());
+        }
+
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keyStore, keyStorePassword.toCharArray());
+
+        // Load your truststore for the MongoDB server certificate
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        try (InputStream trustStoreStream = getClass().getResourceAsStream(trustStorePath)) {
+            trustStore.load(trustStoreStream, keyStorePassword.toCharArray());
+        }
+
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(trustStore);
+
+        // Initialize SSL context with your keystore and truststore
+        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), new java.security.SecureRandom());
+
+        // MongoDB connection settings
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString("mongodb://192.168.2.1:27017/motorist_db?ssl=true"))
+                .applyToSslSettings(builder -> builder.enabled(true).context(sslContext))
+                .build();
+
+        // Return MongoClient with the custom SSL context
+        return MongoClients.create(settings);
+    }
+}
+
